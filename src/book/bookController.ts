67629e7414eb0dd12fceee2084989fa88681d1cd
DHRUB DAS <dhrub27das@gmail.com>
2024-07-25 13:53:21 +0530

    Update Book Controller

--- src/book/bookController.ts
+++ src/book/bookController.ts
@@ -64,6 +64,61 @@ const createBook = async(req:Request,res:Response,next:NextFunction)=>{
 }
 
 const updateBook = async(req:Request,res:Response,next:NextFunction)=>{
+    const {title,genre} = req.body;
+    const bookId = req.params.bookId; 
+    const book =  await bookModel.findOne({_id:bookId});
+    if(!book){
+        return next(createHttpError(401,"Book Not Found"));
+    }
+    //checking if the user has access to the book
+    const _req = req as AuthRequest;
+    if(book.author.toString() != _req.userId){
+        return next(createHttpError(403,"You cannot update others Book. "))            //403--> u are a loggedin user but donot have the access to these books
+    }
+
+    //check if the image field exists or not
+      const files = req.files as { [fieldname: string]: Express.Multer.File[] };
+    let completeCoverImage = "";
+    if (files.coverImage) {
+        const filename = files.coverImage[0].filename;
+        const converMimeType = files.coverImage[0].mimetype.split("/").at(-1);
+        // send files to cloudinary
+        const filePath = path.resolve(
+            __dirname,
+            "../../public/data/uploads/" + filename
+        );
+        completeCoverImage = filename;
+        const uploadResult = await cloudinary.uploader.upload(filePath, {
+            filename_override: completeCoverImage,
+            folder: "book-covers",
+            format: converMimeType,
+        });
+
+        completeCoverImage = uploadResult.secure_url;
+        await fs.promises.unlink(filePath);
+    }
+     // check if file field exists or not.
+     let completeFileName = "";
+     if (files.file) {
+         const bookFilePath = path.resolve(
+             __dirname,
+             "../../public/data/uploads/" + files.file[0].filename
+         );
+ 
+         const bookFileName = files.file[0].filename;
+         completeFileName = bookFileName;
+ 
+         const uploadResultPdf = await cloudinary.uploader.upload(bookFilePath, {
+             resource_type: "raw",
+             filename_override: completeFileName,
+             folder: "book-pdfs",
+             format: "pdf",
+         });
+ 
+         completeFileName = uploadResultPdf.secure_url;
+         await fs.promises.unlink(bookFilePath);
+     }
+
 
 }
    
